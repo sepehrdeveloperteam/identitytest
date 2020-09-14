3e2196cdc26dbd0d61c6a5d4c44c3051f761e052
Reza <Reza@DESKTOP-P7CMIPJ>
2020-09-15 01:20:33 +0430

    Add project files.

---
+++ Data/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace identitytest.Data
+{
+    public class ApplicationUser : IdentityUser
+
+    {
+
+        public string CustomProp { get; set; }
+
+    }
+}
---
+++ Data/Migrations/20200914204404_testcustom01.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace identitytest.Data.Migrations
+{
+    public partial class testcustom01 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CustomProp",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustomProp",
+                table: "AspNetUsers");
+        }
+    }
+}
